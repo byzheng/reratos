b71cf12b795aba6f77433faf4a98d1d80a1b2752
Bangyou Zheng <zheng.bangyou@gmail.com>
2025-10-09 23:01:30 +1000

    Add testthat

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,9 +2,5 @@ library(testthat)
 library(reratos)
 library(httptest2)
 
-# Get the test data
-a <- capture_requests({
-    result <- get_resources(limit = 2, skip = 0, sort = "@date:asc")
-})
 
 test_check("reratos")
